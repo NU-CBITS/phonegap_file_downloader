5881c5fefb55032697570113ab7c404b154cbb8d
Jeanie Chung <jeanie.chung@gmail.com>
2014-09-24 15:14:27 -0500

    Major refactoring, no more global vars, added options to turn on/off progress bars.

--- README.md
+++ README.md
@@ -28,39 +28,73 @@ cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-file-t
 Usage:
 ---------------
 
-Config:
+Initial Setup:
 -------
 Upon initial app installation, any files that you will want to insert into content will have to be downloaded from a remote server. 
 
-The URLs to the files can be changed by modifying the array `downloaderGlobal.download_links`.
+If using the Downloader with a cordova phone, you will want to first invoke the method Downloader.getFileSystem as to set the rootpath of the phone where the files will be downloaded/retrieved.
 
-The order in the array should correspond to the numerical ID you will use to identify the media in the content.
+The URLs to the files can be changed by invoking the method "downloader.setDownloadLinks(links)" with 'links' as an array or a single URL.
 
 For example:
 ```
-downloaderGlobal.download_links = [
-		"http://example.com/video_funny.mp4",
-		"http://example.com/video_mediocre.mp4",
-		"http://example.com/video_sad.mp4"
-	]
+var dl = new Downloader();
+dl.getFileSystem();
+var links = [
+		"http://example.com/funny.mp4",
+		"http://example.com/mediocre.mp4",
+		"http://example.com/sad.mp4"
+	];
+dl.setDownloadLinks(links);
 ```
-will correspond to video1, video2, and video3, respectively.
+
+Config:
+-------
+
+Config settings can be found in the Downloader.config object. Mutable values include text (for internationalization considerations) and options to configure download counters/progress bars.
+
+Text fields include:
+```
+        textDownloadComplete: 'Download Complete!',
+        textDownloading: 'Downloading',
+        textFile: 'file',
+        textFiles: 'files',
+        textDownloadingError: 'Something went wrong with the download and a report has been sent.',
+        textDownloadButton: 'Download',
+        textMissingPlugin: 'File transfer plug in is missing. Downloads may not be complete.',
+        textMissingContent: 'Please download the most recent content.',
+        textUnavailableMedia: '<p>This media is unavailable.</p>',
+        textUnsupportedFileType: 'That file type is not currently supported.',
+```
+
+If you would like to enable/disable the downloader counter or progress bars, you can set the boolean as follows:
+
+```
+dl.config.progressBar = true/false;
+dl.config.downloadCounter = true/false;
+```
+
+If using the download counter/progress bar, you can also set the ID of which DOM elements for the Downloader to manipulate. By default, they are set as 'downloadCounter' and 'progressContainer'.
+
+```
+dl.config.downloadCounterId = 'myDownloadId';
+dl.config.progressBarId = 'myProgressId';
+```
 
 Replacing static content with media:
 -------
 The files.insert method currently takes "video" and "audio" as arguments of file types. 
 
-This will replace any static content of [filetype][numberId] with your chosen download links above. The number id "1" and "01" are functionally the same, meaning they will result in the same file but.
+This will replace any static content of video_[filename]_ with your chosen download links above. The filename in the brackets corresponds to the file name of the downloaded video.
 
 For example
 ```
 var downloader = new Downloader();
-sampleText = "lorem ipsum video0 lorem lorem video 01 lorem video1";
+sampleText = "lorem ipsum video_mediocre.mp4_ lorem lorem video 01 lorem video_funny.mp4_";
 
 sampleText = downloader.insert('video',sampleText);
 ```
-
-will result in video0 playing the "funny" video and video1 and video01 playing the "mediocre" video.
+This will result in video_mediocre.mp4_ displaying the mediocre.mp4 and video_funny.mp4_ displaying funny.mp4
 
 Downloading Single/Multiple Files
 ---------------------------------
@@ -71,9 +105,9 @@ Then you can call the function downloadMultiple() after creating a new instance 
 ```
 var downloader = new Downloader();
 downloaderGlobal.download_links = [
-		"http://example.com/video_funny.mp4",
-		"http://example.com/video_mediocre.mp4",
-		"http://example.com/video_sad.mp4"
+		"http://example.com/funny.mp4",
+		"http://example.com/mediocre.mp4",
+		"http://example.com/sad.mp4"
 	];
 downloader.downloadMultiple();
 ```
@@ -89,7 +123,7 @@ Progress Bars
 -------------
 Where you want a progress bar, add the following markup:
 ```
-<div id="progressContainer" style="display: none;">
+<div id="passThisIdToConfig" style="display: none;">
 </div>
 ```
 and the following CSS
@@ -109,6 +143,10 @@ Error Messages
 FileTransfer is not defined:
 Make sure that the cordova.js file is included in the head
 `<script src="cordova.js"></script>`
+
+
+Download Source Error:
+Make sure the phone is connected to a data connection.
 
 
 
--- file-downloader.js
+++ file-downloader.js
@@ -1,54 +1,56 @@
 var Downloader = function Downloader() {
     this.className = "Downloader";
-};
-
-var downloaderGlobal = {
-    completionTally: 0,
-    failureTally: 0,
-    rootdir: '',
-    textDownloadComplete: 'Download Complete!',
-    textDownloading: 'Downloading',
-    textFile: 'file',
-    textFiles: 'files',
-    textDownloadingError: 'Something went wrong with the download and a report has been sent.',
-    textDownloadButton: 'Download',
-    textMissingPlugin: 'File transfer plug in is missing. Downloads may not be complete.',
-    textMissingContent: 'Please download the most recent content.',
-    textUnavailableMedia: '<p>This media is unavailable.</p>',
-    textUnsupportedFileType: 'That file type is not currently supported.',
-    download_links: [
-        "http://techslides.com/demos/sample-videos/small.mp4"
-    ]
+    this.config = {
+        completionTally: 0,
+        failureTally: 0,
+        rootdir: '',
+        textDownloadComplete: 'Download Complete!',
+        textDownloading: 'Downloading',
+        textFile: 'file',
+        textFiles: 'files',
+        textDownloadingError: 'Something went wrong with the download and a report has been sent.',
+        textDownloadButton: 'Download',
+        textMissingPlugin: 'File transfer plug in is missing. Downloads may not be complete.',
+        textMissingContent: 'Please download the most recent content.',
+        textUnavailableMedia: '<p>This media is unavailable.</p>',
+        textUnsupportedFileType: 'That file type is not currently supported.',
+        download_links: [
+            "http://techslides.com/demos/sample-videos/small.mp4"
+        ],
+        test: 'testing!',
+        downloadCounter: true,
+        progressBar: true,
+        downloadCounterId: 'downloadCounter',
+        progressBarId: 'progressContainer'
+    };
 };
 
 Downloader.prototype = {
-     
     getFileSystem: function() {
+        var self = this;
         document.addEventListener('deviceready', function() {
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
-        function fileSystemSuccess(fileSystem) {
-            downloaderGlobal.rootdir = fileSystem.root.toURL();
-        }
-        function fileSystemFail(evt) {
-            //Unable to access file system
-            alert(evt.target.error.code);
-        }
-      });
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
+            function fileSystemSuccess(fileSystem) {
+                self.config.rootdir = fileSystem.root.toURL();
+            }
+            function fileSystemFail(evt) {
+                //Unable to access file system
+                alert(evt.target.error.code);
+            }
+        });
     },
 
     test: function() {
-        return "test";
+        return this.config.test;
     },
 
     setDownloadLinks: function(links) {
         if (links instanceof Array) {
-            downloaderGlobal.download_links = links;
+            this.config.download_links = links;
         }
         else {
-            downloaderGlobal.download_links = links;
+            this.config.download_links = links;
         }
-        alert(downloaderGlobal.download_links);
-
     },
     downloadSingle: function(url,folder) {
         var fpSingle = '';
@@ -56,45 +58,45 @@ Downloader.prototype = {
         var fileName = url.substr(url.lastIndexOf('/') + 1);
         fileName = fileName.split('.')[0];
         if (folder) {
-            fpSingle = downloaderGlobal.rootdir + folder + fileName + "1" + "." + ext; // file path and name
+            fpSingle = this.config.rootdir + folder + fileName + "1" + "." + ext; // file path and name
         }
         else {
-            fpSingle = downloaderGlobal.rootdir + fileName + "." + ext; // file path and name
+            fpSingle = this.config.rootdir + fileName + "." + ext; // file path and name
         }
         localStorage.setItem('fpSingle',fpSingle);
 
         // call file transfer function
-        filetransfer(url, fpSingle, 1);
-
+        this.filetransfer(url, fpSingle, 1);
+        var self = this;
         setTimeout(function() {
-            if (downloaderGlobal.failureTally !== 0) {
-                alert(downloaderGlobal.textDownloadingError);
+            if (self.config.failureTally !== 0) {
+                alert(self.config.textDownloadingError);
             }
-            downloaderGlobal.failureTally = 0;
+            self.config.failureTally = 0;
         }, 500);
     },
     downloadMultiple: function() {
         // the order that these links are listed is the order that corresponds to the content.
         // e.g. the first link in this array will correspond to the string 'video01' in the content.
-        var dl_links = downloaderGlobal.download_links;
+        var dl_links = this.config.download_links;
         var numDownloads = dl_links.length;
         fp = [];
         for (var i = 0; i < numDownloads; i++) {
             var fileName = dl_links[i].substr(dl_links[i].lastIndexOf('/') + 1);
             fileName = fileName.split('.')[0];
             var ext = dl_links[i].substr(dl_links[i].lastIndexOf('.') + 1);
-            fp.push(downloaderGlobal.rootdir + fileName + "." + ext); // file path and name
+            fp.push(this.config.rootdir + fileName + ".mp4"); // file path and name
             localStorage.setItem('fp',JSON.stringify(fp));
 
             // call file transfer function
-            filetransfer(dl_links[i], fp[i], numDownloads);
+            this.filetransfer(dl_links[i], fp[i], numDownloads);
         }
-
+        var self = this;
         setTimeout(function() {
-            if (downloaderGlobal.failureTally !== 0) {
-                alert(downloaderGlobal.textDownloadingError);
+            if (self.config.failureTally !== 0) {
+                alert(self.config.textDownloadingError);
             }
-            downloaderGlobal.failureTally = 0;
+            self.config.failureTally = 0;
         }, 500);
     },
     findElements: function(fileType,content) {
@@ -102,7 +104,7 @@ Downloader.prototype = {
 
         // currently supported file types: video, audio
         // should follow the format of "video_small.mp4"
-        var reg = new RegExp(fileType+"_(.*)","g");
+        var reg = new RegExp(fileType+"_(.*)_","g");
         var found = String(content.match(reg));
 
         elements.push(found);
@@ -113,12 +115,11 @@ Downloader.prototype = {
     replaceElements: function(fileType,content,elements) {
         var elemFileNames = [];
         var elemNew = [];
-
         console.log("elements: "+elements);
         for (i = 0; i < elements.length; i++) {
             // access local storage if files have already been downloaded
             if (typeof localStorage.fp === 'undefined') {
-                alert(downloaderGlobal.textMissingContent);
+                alert(this.config.textMissingContent);
                 return content;
             }
             else if (elements[0] === "null") {
@@ -128,21 +129,20 @@ Downloader.prototype = {
                 fp = JSON.parse(localStorage.fp);
             }
             // create an array of the ID numbers
-            var elemFileName = new RegExp(fileType+"_(.*)");
-
+            var elemFileName = new RegExp(fileType+"_(.*)_");
             elemFileNames.push(elements[i].match(elemFileName)[1]);
-            // create the corresponding video tag, referencing the index number of the file path array
-            var elemTag;
-            var fileIndex = fp.indexOf(elemFileNames[i]);
+            var fileIndex = fp.indexOf(this.config.rootdir+elemFileNames[i]);
+            console.log(this.config.rootdir+elemFileNames[i]);
             var fileSrc = fp[fileIndex];
                 if (fileType === "video") {
                     elemTag = "<video controls style='max-width:100%;'><source type='video/mp4' src='"+fileSrc+"'/></video>";
+                    console.log(elemTag);
                 }
                 else if (fileType === "audio") {
                     elemTag = "<audio controls style='max-width:100%;'><source type='audio/mpeg' src='"+fileSrc+"'/></audio>";
                 }
                 else {
-                    alert(downloaderGlobal.textUnsupportedFileType);
+                    alert(this.config.textUnsupportedFileType);
                     return content;
                 }
             elemNew.push(elemTag);
@@ -150,101 +150,110 @@ Downloader.prototype = {
                 content = content.replace(elements[i],elemNew[i]);
             }
             else {
-                content = content.replace(elements[i],downloaderGlobal.textUnavailableMedia);
+                content = content.replace(elements[i],this.config.textUnavailableMedia);
             }
         }
         return content;
     },
 
     insert: function(fileType,content) {
-        var dl = new Downloader();
-        thingsToReplace = dl.findElements(fileType,content);
-        content = dl.replaceElements(fileType,content,thingsToReplace);
+        var thingsToReplace = this.findElements(fileType,content);
+        var content = this.replaceElements(fileType,content,thingsToReplace);
 
         return content;
+    },
+    constructDownloadCounter: function(ftObject,numDownloads) {
+        var self = this;
+        var downloadContainer = document.getElementById(self.config.downloadCounterId).innerHTML = self.config.textDownloading + " " +numDownloads+ " " + self.config.textFiles + "...";
+        console.log(downloadContainer);
+        ftObject.onprogress = function(progressEvent) {
+            
+            if (progressEvent.lengthComputable) {
+                var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+                if (perc === 100) {
+                    setTimeout(function() {
+                        var numDownloadsRemaining = numDownloads - self.config.completionTally;
+
+                        if (numDownloads > 1) {
+                            downloadContainer.innerHTML = self.config.textDownloading + " "+numDownloadsRemaining+" "+self.config.textFiles + "...";
+                        }
+                        else if (numDownloadsRemaining === 1) {
+                            downloadContainer.innerHTML = self.config.textDownloading + " 1 " + self.config.textFile + "...";
+                        }
+                        else {
+                            progressContainer.setAttribute("style","display: none");
+                        }
+                    }, 1000);
+                }
+            }
+        };
+    },
+
+    constructProgressBar: function(ftObject, numDownloads) {
+        // set the ID of your html progress bar div/span in the config variables
+
+
+        var progressContainer = document.getElementById(this.config.progressBarId);
+            progressContainer.setAttribute("style","display: block"); 
+        var progress = document.createElement("div");
+            progress.setAttribute("class","progress");
+            progressContainer.appendChild(progress);
+        var progressbar = document.createElement("div");
+            progressbar.setAttribute("class","progress-bar progress-bar-striped");
+            progressbar.setAttribute("aria-valuemin","0");
+            progressbar.setAttribute("aria-valuemax","100");
+            progressbar.setAttribute("role","progressbar");
+            progress.appendChild(progressbar);
+
+        ftObject.onprogress = function(progressEvent) {
+            
+            if (progressEvent.lengthComputable) {
+                var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+                progressbar.setAttribute("style","width: "+perc+"%");
+                progressbar.setAttribute("aria-valuenow",perc);
+
+                if (perc === 100) {
+                    setTimeout(function() {
+                        progress.setAttribute("class","progress fade");
+
+                        progressContainer.setAttribute("style","display: none");
+                    }, 1000);
+                }
+            }
+        };
+    },
+
+    filetransfer: function(file,filepath,numFiles) {
+        var fileTransfer = new FileTransfer();
+        var self = this;
+        self.config.completionTally = 0;
+        if (self.config.downloadCounter === true) {
+            self.constructDownloadCounter(fileTransfer,numFiles);
+        }
+        if (self.config.progressBar === true) {
+            self.constructProgressBar(fileTransfer,numFiles);
+        }
+        if (typeof FileTransfer === 'undefined') {
+            alert(self.config.textMissingPlugin);
+            return;
+        }
+        
+        fileTransfer.download(
+            file,
+            filepath,
+            function(entry) {
+                console.log("download complete: " + entry.fullPath);
+                self.config.completionTally++;
+
+                if (self.config.completionTally === numFiles) {
+                    alert(self.config.textDownloadComplete);
+                }
+            },
+            function(error) {
+                console.log("download error source " + error.source);
+                self.config.failureTally++;
+                console.log(self.config.failureTally);
+            }
+        );
     }
 };
-
-function constructProgressBar (ftObject, numDownloads) {
-    // Where you want a progress bar, add the following markup:
-    //  <div id="progressContainer" style="display: none;">
-    //  </div>
-    // and the following CSS
-    // div.progress {
-    //     opacity: 1;
-    //     transition: opacity 1s;
-    // }
-    // div.progress.fade {
-    //     display: none;
-    // }
-
-    document.getElementById("download-counter").innerHTML = downloaderGlobal.textDownloading + " " +numDownloads+ " " + downloaderGlobal.textFiles + "...";
-
-    var progressContainer = document.getElementById('progressContainer');
-        progressContainer.setAttribute("style","display: block"); 
-    var progress = document.createElement("div");
-        progress.setAttribute("class","progress");
-        progressContainer.appendChild(progress);
-    var progressbar = document.createElement("div");
-        progressbar.setAttribute("class","progress-bar progress-bar-striped");
-        progressbar.setAttribute("aria-valuemin","0");
-        progressbar.setAttribute("aria-valuemax","100");
-        progressbar.setAttribute("role","progressbar");
-        progress.appendChild(progressbar);
-
-    ftObject.onprogress = function(progressEvent) {
-        
-        if (progressEvent.lengthComputable) {
-            var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
-            progressbar.setAttribute("style","width: "+perc+"%");
-            progressbar.setAttribute("aria-valuenow",perc);
-
-            if (perc === 100) {
-                setTimeout(function() {
-                    progress.setAttribute("class","progress fade");
-
-                    var numDownloadsRemaining = numDownloads - downloaderGlobal.completionTally;
-
-                    if (numDownloads > 1) {
-                        document.getElementById("download-counter").innerHTML = downloaderGlobal.textDownloading + " "+numDownloadsRemaining+" "+downloaderGlobal.textFiles + "...";
-                    }
-                    else if (numDownloadsRemaining === 1) {
-                        document.getElementById("download-counter").innerHTML = downloaderGlobal.textDownloading + " 1 " + downloaderGlobal.textFile + "...";
-                    }
-                    else {
-                        progressContainer.setAttribute("style","display: none");
-                    }
-                }, 1000);
-            }
-        }
-    };
-}
-
-function filetransfer(file,filepath,numFiles) {
-    var fileTransfer = new FileTransfer();
-    downloaderGlobal.completionTally = 0;
-    constructProgressBar(fileTransfer,numFiles);
-
-    if (typeof FileTransfer === 'undefined') {
-        alert(downloaderGlobal.textMissingPlugin);
-        return;
-    }
-    
-    fileTransfer.download(
-        file,
-        filepath,
-        function(entry) {
-            console.log("download complete: " + entry.fullPath);
-            downloaderGlobal.completionTally++;
-
-            if (downloaderGlobal.completionTally === numFiles) {
-                alert(downloaderGlobal.textDownloadComplete);
-            }
-        },
-        function(error) {
-            console.log("download error source " + error.source);
-            downloaderGlobal.failureTally++;
-            console.log(downloaderGlobal.failureTally);
-        }
-    );
-}
